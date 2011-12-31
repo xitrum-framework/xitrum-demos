6abf6a8571bd54a717a0ecf30c9719bee845e31a
Ngoc Dao <ngocdaothanh@gmail.com>
2011-12-31 16:28:04 +0900

    Use Jade templates

--- src/main/scala/quickstart/action/AppAction.scala
+++ src/main/scala/quickstart/action/AppAction.scala
@@ -1,51 +1,12 @@
 package quickstart.action
 
 import xitrum.Action
-import xitrum.view.DocType
 
 trait AppAction extends Action {
-  override def layout = DocType.html5(
-    <html>
-      <head>
-        {antiCSRFMeta}
-        {xitrumCSS}
+  override def layout = renderScalateTemplateToString(classOf[AppAction])
 
-        <meta content="text/html; charset=utf-8" http-equiv="content-type" />
-        <title>Welcome to Xitrum</title>
-
-        <link type="image/vnd.microsoft.icon" rel="shortcut icon" href={urlForPublic("../favicon.ico")} />
-
-        <link type="text/css" rel="stylesheet" media="all" href={urlForPublic("css/960/reset.css")} />
-        <link type="text/css" rel="stylesheet" media="all" href={urlForPublic("css/960/text.css")} />
-        <link type="text/css" rel="stylesheet" media="all" href={urlForPublic("css/960/960.css")} />
-        <link type="text/css" rel="stylesheet" media="all" href={urlForPublic("css/app.css")} />
-      </head>
-      <body>
-        <div class="container_12">
-          <h1><a href={urlFor[IndexAction]}>Welcome to Xitrum</a></h1>
-
-          <div class="grid_8">
-            <p>{sourceCodeLink}</p>
-            <div id="flash">{jsFlash}</div>
-            {renderedView}
-          </div>
-
-          <div class="grid_4">
-            <h3>Samples</h3>
-            <ul>
-              <li><a href={urlFor[CometAction]}>Comet chat</a></li>
-              <li><a href={urlFor[BoringAction]}>Boring workflow</a></li>
-              <li><a href={urlFor[GreeterAction]}>Greeter workflow</a></li>
-              <li><a href={urlFor[TodoAction]}>Todo list</a></li>
-            </ul>
-          </div>
-        </div>
-        {jsAtBottom}
-      </body>
-    </html>
-  )
-
-  private def sourceCodeLink = {
+  /** Used in layout */
+  def sourceCodeLink = {
     val fullClassName = getClass.getName
     val className     = fullClassName.split("\\.").last
     val desc          = "Source code of %s.scala".format(className)
--- src/main/scala/quickstart/action/IndexAction.scala
+++ src/main/scala/quickstart/action/IndexAction.scala
@@ -5,12 +5,6 @@ import xitrum.annotation.GET
 @GET("/")
 class IndexAction extends AppAction {
   override def execute {
-    renderView(
-      <p>See:</p>
-      <ul>
-        <li>Xitrum: <a href="https://github.com/ngocdaothanh/xitrum">https://github.com/ngocdaothanh/xitrum</a></li>
-        <li>This quickstart: <a href="https://github.com/ngocdaothanh/xitrum-quickstart">https://github.com/ngocdaothanh/xitrum-quickstart</a></li>
-      </ul>
-    )
+    renderScalateView()
   }
 }
