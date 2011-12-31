60b319636ba20fc8d43261a1d1a68f6c287f2c40
Ngoc Dao <ngocdaothanh@gmail.com>
2011-12-31 16:29:46 +0900

    Use Jade templates

--- src/main/scala/quickstart/action/CometAction.scala
+++ src/main/scala/quickstart/action/CometAction.scala
@@ -1,27 +1,10 @@
 package quickstart.action
 
 import xitrum.annotation.GET
-import xitrum.comet.CometPublishAction
-import xitrum.validation.{Required, Validated}
 
 @GET("/comet")
 class CometAction extends AppAction {
   override def execute {
-    jsCometGet("chat", """
-      function(channel, timestamp, body) {
-        var wasScrollAtBottom = xitrum.isScrollAtBottom('#chatOutput');
-        $('#chatOutput').append('- ' + xitrum.escapeHtml(body.chatInput[0]) + '<br />');
-        if (wasScrollAtBottom) xitrum.scrollToBottom('#chatOutput');
-      }
-    """)
-
-    renderView(
-      <h1>Chat</h1>
-      <div id="chatOutput"></div>
-      <form data-postback="submit" action={urlForPostback[CometPublishAction]} data-after="$('#chatInput').val('')">
-        {<input type="hidden" name="channel" value="chat" /> :: Validated}
-        {<input type="text" id="chatInput" name="chatInput" /> :: Required}
-      </form>
-    )
+    renderScalateView()
   }
 }
