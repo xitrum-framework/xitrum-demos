8ba422e562ed3bf1f875d8b4fc20699bfd342340
Ngoc Dao <ngocdaothanh@gmail.com>
2012-01-09 10:04:57 +0900

    Use the term controller&action instead of controller&route

--- src/main/scala/quickstart/controller/Errors.scala
+++ src/main/scala/quickstart/controller/Errors.scala
@@ -3,11 +3,11 @@ package quickstart.controller
 object Errors extends Errors
 
 class Errors extends AppController {
-  val error404 = indirectRoute {
+  val error404 = indirectAction {
     respondInlineView("This is custom 404 page")
   }
 
-  val error500 = indirectRoute {
+  val error500 = indirectAction {
     respondInlineView("This is custom 500 page")
   }
 }
