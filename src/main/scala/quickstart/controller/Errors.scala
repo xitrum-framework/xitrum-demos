f7ae0868653fe1c34f7129a5736907bab46c09ee
Ngoc Dao <ngocdaothanh@gmail.com>
2012-01-10 14:06:32 +0900

    Jade templates can be used with ErrorController

--- src/main/scala/quickstart/controller/Errors.scala
+++ src/main/scala/quickstart/controller/Errors.scala
@@ -4,10 +4,10 @@ import xitrum.ErrorController
 
 class Errors extends AppController with ErrorController {
   def error404 = errorAction {
-    respondInlineView("This is custom 404 page")
+    respondView()
   }
 
   def error500 = errorAction {
-    respondInlineView("This is custom 500 page")
+    respondView()
   }
 }
