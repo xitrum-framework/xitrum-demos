ffa96661289706eb4b2f51f7dffa0431363f8c76
Ngoc Dao <ngocdaothanh@Ngocs-MacBook-Pro.local>
2012-12-02 21:16:49 +0900

    Add Mustache example

--- src/main/scala/demos/controller/Site.scala
+++ src/main/scala/demos/controller/Site.scala
@@ -6,4 +6,12 @@ class Site extends AppController {
   def index = GET {
     respondView()
   }
+
+  def mustache = GET("mustache") {
+    at("name")        = "Chris"
+    at("value")       = 10000
+    at("taxed_value") = 10000 - (10000 * 0.4)
+    at("in_ca")       = true
+    respondView("mustache")
+  }
 }
