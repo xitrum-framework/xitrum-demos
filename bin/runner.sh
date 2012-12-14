d7c5e6e80d0dfaf51bff95510be64fb3a5c81a2b
Ngoc Dao <ngocdaothanh@gmail.com>
2012-12-14 15:03:37 +0900

    Update Xitrum to 1.11

--- bin/runner.sh
+++ bin/runner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # You may need to customize memory config below to optimize for your environment
-JAVA_OPTS='-server -Xms256m -Xmx1024m -XX:MaxPermSize=128m -XX:+UseParallelGC -Djava.awt.headless=true -Dxitrum.mode=production'
+JAVA_OPTS='-Xmx1024m -Xms256m -XX:MaxPermSize=128m -XX:+HeapDumpOnOutOfMemoryError -XX:+AggressiveOpts -XX:+OptimizeStringConcat -XX:+UseFastAccessorMethods -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1 -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly -Djava.awt.headless=true -server -Dxitrum.mode=production'
 
 # Quote because path may contain spaces
 if [ -h $0 ]
@@ -12,12 +12,9 @@ else
 fi
 cd "$ROOT_DIR"
 
-# Put slf4j-api to the front to avoid error (not always occurs):
-# The requested version 1.6 by your slf4j binding is not compatible with [1.5.5, 1.5.6, 1.5.7, 1.5.8]
-#
 # Include ROOT_DIR to do "ps aux | grep java" to get pid easier when
 # starting multiple processes from different directories
-CLASS_PATH="$ROOT_DIR/lib/slf4j-api-1.6.6.jar:lib/*:config"
+CLASS_PATH="$ROOT_DIR/lib/*:config"
 
 # Use exec to be compatible with daemontools:
 # http://cr.yp.to/daemontools.html
