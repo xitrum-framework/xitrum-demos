ad5011caac0c3632009008455b0dfde12cd061e6
Ngoc Dao <ngocdt@ngocdt-ubuntu.(none)>
2013-04-19 18:52:46 +0900

    Demo cache config collecting in Xitrum 2.2

---
+++ src/main/scala/demos/action/CacheDemo.scala
@@ -0,0 +1,39 @@
+package demos.action
+
+import xitrum.annotation.{CacheActionSecond, CachePageSecond, GET}
+
+@CacheActionSecond(10)
+trait TenSecsCacheAction extends AppAction
+
+@GET("cache/action")
+class ActionCacheDemo extends TenSecsCacheAction {
+  beforeFilter {
+    logger.info("Filter is run")
+    true
+  }
+
+  def execute() {
+    respondInlineView(
+      "In production mode, this action is cached 10 seconds. " +
+      "For action cache, filters are run. Please try refreshing and see the log. " +
+      "System.currentTimeMillis(): " + System.currentTimeMillis()
+    )
+  }
+}
+
+@GET("cache/page")
+@CachePageSecond(10)
+class PageCacheDemo extends AppAction {
+  beforeFilter {
+    logger.info("Filter is run")
+    true
+  }
+
+  def execute() {
+    respondInlineView(
+      "In production mode, this page is cached 10 seconds. " +
+      "For page cache, filters are NOT run. Please try refreshing and see the log. " +
+      "System.currentTimeMillis(): " + System.currentTimeMillis()
+    )
+  }
+}
