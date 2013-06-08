5be129fed9e27310ebab3f19b69ea59e0f06a8df
Dao Ngoc <ngoc@Daos-MacBook-Pro.local>
2013-06-08 11:34:11 +0900

    Update Scala from 2.10.1 to 2.10.2

--- src/main/scala/demos/action/ActionActorDemo.scala
+++ src/main/scala/demos/action/ActionActorDemo.scala
@@ -10,7 +10,7 @@ class ActionActorDemo extends ActionActor with AppAction {
   def execute() {
     // See Akka doc about scheduler
     import context.dispatcher
-    context.system.scheduler.scheduleOnce(3 seconds, self, System.currentTimeMillis)
+    context.system.scheduler.scheduleOnce(3.seconds, self, System.currentTimeMillis)
 
     // See Akka doc about "become"
     context.become {
