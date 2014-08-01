dcc1980d6261435f231c45b96ee418a3addab856
Ngoc Dao <ngocdaothanh@gmail.com>
2014-08-01 12:53:33 +0900

    Fix #13 Get source file: Use Javassist instead of creating exception

--- src/main/scala/demos/action/AppAction.scala
+++ src/main/scala/demos/action/AppAction.scala
@@ -3,13 +3,5 @@ package demos.action
 import xitrum.Action
 
 trait AppAction extends Action {
-  override def layout = {
-    // Get file path of the current action by creating an exception.
-    // This is slow but OK for demos.
-    //
-    // stack(0) is AppAction, stack(1) is current extended Action
-    val stack = new RuntimeException("").getStackTrace()
-    at("currentActionFileName") = stack(1).getFileName()
-    renderViewNoLayout[AppAction]()
-  }
+  override def layout = renderViewNoLayout[AppAction]()
 }
--- src/main/scala/demos/action/SourceRenderer.scala
+++ src/main/scala/demos/action/SourceRenderer.scala
@@ -1,16 +1,47 @@
 package demos.action
 
 import scala.util.control.NonFatal
+
+import javassist.{ClassPool, ClassClassPath}
+import xitrum.Config.xitrum.cache
 import xitrum.util.Loader
 
-object SourceRenderer {
-  def render(path: String) = {
-    val src =
-      try {
-        Loader.stringFromFile(path)
-      } catch {
-        case NonFatal(_) => "Could not load file: " + path
-      }
-    src
+object Source {
+  private val classPool = {
+    val ret       = ClassPool.getDefault
+    val classPath = new ClassClassPath(getClass)
+    ret.insertClassPath(classPath)
+    ret
+  }
+
+  def getSourceFileName(className: String) = {
+    cache.get(className) match {
+      case Some(fileName) =>
+        fileName
+
+      case None =>
+        val ctClass   = classPool.get(className)
+        val classFile = ctClass.getClassFile
+        val fileName  = classFile.getSourceFile
+        cache.put(className, fileName)
+        fileName
+    }
+  }
+
+  def getFileContent(path: String) = {
+    cache.get(path) match {
+      case Some(content) =>
+        content.toString
+
+      case None =>
+        try {
+          val content = Loader.stringFromFile(path)
+          cache.put(path, content)
+          content
+        } catch {
+          case NonFatal(_) =>
+            "Could not load file: " + path
+        }
+    }
   }
 }
